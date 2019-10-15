c985e7cc753bc7c731a0fbc9bd94401746637ae8
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-10-15 17:47:25 -0600

    created siteName and siteUrl variable to get form values

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save Bookmark
 function saveBookmark(e){
+    //Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteURL = document.getElementById('siteURL').value;
+    console.log(siteName);
     
     // Prevent form from submitting
     e.preventDefault();
